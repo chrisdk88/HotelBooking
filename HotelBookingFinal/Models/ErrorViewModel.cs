db528f2d3fd6aeb3734b7d88046f1c03a15c1f97
Chris Sørensen <chso.skp@edu.mercantec.dk>
2023-08-30 14:25:44 +0200

    Add project files.

---
+++ HotelBookingFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelBookingFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
