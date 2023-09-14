67b055280a75e78b75c18f166bad9c4cf6987137
Turbobent <kasp728h@edu.mercantec.dk>
2023-09-14 12:49:16 +0200

    login and footer update

--- Client/Pages/Login.razor.cs
+++ Client/Pages/Login.razor.cs
@@ -2,6 +2,12 @@
 {
     public partial class Login
     {
-
-    }
+		 string email;
+		 string password;
+		 string errorMessage;
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			
+		}
+	}
 }
