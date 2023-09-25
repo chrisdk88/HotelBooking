5cf77912ff6a4ec3f46b7f965afb14a2a4051df5
Turbobent <kasp728h@edu.mercantec.dk>
2023-09-25 12:58:25 +0200

    userid

--- Client/Pages/Login.razor.cs
+++ Client/Pages/Login.razor.cs
@@ -1,45 +1,60 @@
-﻿using Microsoft.JSInterop;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using Models;
 using System.Net.Http.Json;
-using System.Security.Cryptography.X509Certificates;
 
 namespace Client.Pages
 {
-	public partial class Login
-	{
-		private User LoginUser = new User();
-		public string errorMessage;
-		private int customerId;
-
+    public partial class Login
+    {
+        private User LoginUser = new User();
+        public string errorMessage;
+        private int customerId;
+        public Customer customeruser;
+        public bool isLoggedIn { get; set; }
+        public static class GlobalAuthState
+        {
+            public static bool IsLoggedIn { get; set; } = false;
+            public static int UserId { get; set; } = -1; // Example: You can store the user ID if needed
+        }
         private async Task HandleLogin()
-		{
-			if (!string.IsNullOrWhiteSpace(LoginUser.email) && !string.IsNullOrWhiteSpace(LoginUser.password))
-			{
-				string email = LoginUser.email;
-				string password = LoginUser.password;
+        {
+            if (!string.IsNullOrWhiteSpace(LoginUser.email) && !string.IsNullOrWhiteSpace(LoginUser.password))
+            {
+                string email = LoginUser.email;
+                string password = LoginUser.password;
 
-				// Make an HTTP request to authenticate the user.
-				Customer Customeruser = await Http.GetFromJsonAsync<Customer>($"https://localhost:7285/api/Customers/{email}/{password}");
+                // Make an HTTP request to authenticate the user.
+                customeruser = await Http.GetFromJsonAsync<Customer>($"https://localhost:7285/api/Customers/{email}/{password}");
 
-				if (Customeruser != null )
-				{
-					// Handle a successful login, navigate to the users dashboard.
-					
-					 NavigationManager.NavigateTo("/");
-					  customerId = (int)Customeruser.id;
-				}
-				else
-				{
-                    Console.WriteLine("abc");
+                if (customeruser != null)
+                {
+                    // Handle a successful login, navigate to the users dashboard.
+                    NavigationManager.NavigateTo("/");
+                    customerId = (int)customeruser.id;
+                    isLoggedIn = true;
+                    GlobalAuthState.IsLoggedIn = true;
+                }
+                else
+                {
                     // Invalid credentials. Display an error message.
                     errorMessage = "Invalid credentials. Please check your email and password.";
-				}
-			}
+                }
+            }
             else
             {
                 // Handle empty or invalid input
                 errorMessage = "Please enter your email and password.";
             }
         }
-	}
+
+        public void Logout()
+        {
+            isLoggedIn = false;
+            customeruser = null; // Clear the authenticated user
+            GlobalAuthState.IsLoggedIn = false;
+            GlobalAuthState.UserId = -1;
+        }
+    }
 }
+
---
+++ Client/Shared/Utilities/GlobalAuthState.cs
@@ -0,0 +1,8 @@
+﻿namespace Client.Shared.Utilities
+{
+    public static class GlobalAuthState
+    {
+        public static bool IsLoggedIn { get; set; } = false;
+        public static int UserId { get; set; } = -1; // Example: You can store the user ID if needed
+    }
+}
