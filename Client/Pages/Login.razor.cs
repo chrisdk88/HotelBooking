61d511d5918e6e36e2b40f3f299bb26296a3be02
Turbobent <kasp728h@edu.mercantec.dk>
2023-09-27 09:43:49 +0200

    both admin and customers can login

--- Client/Pages/Login.razor.cs
+++ Client/Pages/Login.razor.cs
@@ -7,46 +7,57 @@ using System.Net.Http.Json;
 using Client.Shared.Utilities;
 namespace Client.Pages
 {
-    public partial class Login
-    {
-        private User LoginUser = new User();
-        public string errorMessage;
-        public Customer customeruser;
-        public Admin adminuser;
+	public partial class Login
+	{
+		private User LoginUser = new User();
+		public string errorMessage;
+		public Customer customeruser;
+		public Admin adminuser;
 		private async Task HandleLogin()
 		{
 			if (!string.IsNullOrWhiteSpace(LoginUser.email) && !string.IsNullOrWhiteSpace(LoginUser.password))
 			{
 				string email = LoginUser.email;
 				string password = LoginUser.password;
-
-				// Make an HTTP request to authenticate the admin.
-				adminuser = await Http.GetFromJsonAsync<Admin>($"https://localhost:7285/api/Admins/{email}/{password}");
-
-				if (adminuser != null)
+				try
 				{
-					NavigationManager.NavigateTo("/");
-					GlobalAuthState.UserId = adminuser.id;
-					StateHasChanged();
+					// Make an HTTP request to authenticate the admin.
+					adminuser = await Http.GetFromJsonAsync<Admin>($"https://localhost:7285/api/Admins/{email}/{password}");
+					if (adminuser != null)
+					{
+						NavigationManager.NavigateTo("/");
+						GlobalAuthState.UserId = adminuser.id;
+						Console.WriteLine(adminuser);
+
+						StateHasChanged();
+						return;
+					}
 				}
-				else
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Exception: {ex.Message}");
+					errorMessage = "invalid email or username";
+				}
+
+				try
 				{
 					// Admin authentication failed; let's try customer authentication.
 					customeruser = await Http.GetFromJsonAsync<Customer>($"https://localhost:7285/api/Customers/{email}/{password}");
-
 					if (customeruser != null)
 					{
 						NavigationManager.NavigateTo("/");
 						GlobalAuthState.UserId = customeruser.id;
 						StateHasChanged();
-					}
-					else
-					{
-						// Both admin and customer authentication failed.
-						errorMessage = "Invalid credentials. Please check your email and password.";
+						Console.WriteLine(customeruser);
 					}
 				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Exception: {ex.Message}");
+					errorMessage = "invalid email or username";
+				}
 			}
+
 			else
 			{
 				// Handle empty or invalid input
@@ -54,14 +65,19 @@ namespace Client.Pages
 			}
 		}
 
-
 		public void Logout()
-        {
-			customeruser = null; // Clear the authenticated user
-			GlobalAuthState.UserId = null;
-			StateHasChanged();
+		{
+			try
+			{
+				customeruser = null; // Clear the authenticated user
+				GlobalAuthState.UserId = null;
+				StateHasChanged();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Exception: {ex.Message}");
+			}
 		}
-	
 	}
 }
 
