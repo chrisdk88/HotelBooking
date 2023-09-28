e20c9bf65d0917126318f1a761c79540549b2a07
chrisdk88 <108925053+chrisdk88@users.noreply.github.com>
2023-09-28 11:19:30 +0200

    Updated admin page

---
+++ Client/Pages/AdminDashboard.razor.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System.Net.Http.Json;
+
+namespace Client.Pages
+{
+    public partial class AdminDashboard
+    {
+        public async Task<List<Room>?> GetListOfRooms()
+        {
+            var allRooms = await Http.GetFromJsonAsync<List<Room>>("https://localhost:7285/api/Rooms");
+            foreach (var item in allRooms)
+            {
+                Console.WriteLine(item);
+            }
+            return allRooms;
+        }
+    }
+}
