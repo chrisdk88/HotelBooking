8320e43c297b801af2f5e6b4c8e3a2b873951f90
chrisdk88 <108925053+chrisdk88@users.noreply.github.com>
2023-10-05 14:46:10 +0200

    Updated

--- Client/Pages/AdminDashboard.razor.cs
+++ Client/Pages/AdminDashboard.razor.cs
@@ -1,15 +1,34 @@
-﻿using Models;
+﻿using Client.Shared.Utilities;
+using Models;
 using System.Net.Http.Json;
+using System.Text;
 
 namespace Client.Pages
 {
     public partial class AdminDashboard
     {
-        public async Task<List<Room>?> GetListOfRooms()
+        private HttpClient client = new HttpClient() { BaseAddress = new Uri("https://localhost:7285/") };
+        public class input 
         {
-            var allRooms = await Http.GetFromJsonAsync<List<Room>>("https://localhost:7285/api/Rooms");
+            public int typeId;
+            public int roomNumber;
+        }
+        public async Task addRoom()
+        {
+            Room room = new Room()
+            {
+                roomNum = i.roomNumber,
+                typeId = (uint)i.typeId
+            };
+            // Serialize the userModel to JSON
+            string json = System.Text.Json.JsonSerializer.Serialize(room);
+            Console.WriteLine(json);
 
-            return allRooms;
+            // Create HttpContent with JSON data
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            // Use HttpClient to send a POST request to your Swagger API
+            var response = await client.PostAsync("api/Rooms", content);
         }
     }
 }
